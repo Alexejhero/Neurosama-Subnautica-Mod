3165f1caf3145801138343d4fa48a6fe32490fab
Govorunb <Govorunb@users.noreply.github.com>
2023-09-18 10:28:01 +1000

    hack fix for cyclops destroying itself with an aquarium

---
+++ SCHIZO/Patches/FixAquariumCollisionsPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace SCHIZO.Patches;
+
+[HarmonyPatch]
+public static class FixAquariumCollisionsPatch
+{
+    [HarmonyPatch(typeof(DealDamageOnImpact), nameof(DealDamageOnImpact.OnCollisionEnter))]
+    [HarmonyPrefix]
+    public static bool FixAquariumCollisionDamage(DealDamageOnImpact __instance, Collision collision)
+        => __instance.gameObject.GetComponent<SubRoot>() && !collision.gameObject.GetComponent<Aquarium>();
+
+    [HarmonyPatch(typeof(SubRoot), nameof(SubRoot.OnCollisionEnter))]
+    [HarmonyPrefix]
+    public static bool FixAquariumCollisionSound(SubRoot __instance, Collision col) => !col.gameObject.GetComponent<Aquarium>();
+}
