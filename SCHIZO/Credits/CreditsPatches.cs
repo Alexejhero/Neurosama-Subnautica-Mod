030c5a80ca1d0fb34d611d1b87ff6579e1d85817
DrGlitchy <98728356+hitman13145@users.noreply.github.com>
2023-09-19 10:10:29 +1000

    Update CreditsPatches.cs

--- SCHIZO/Credits/CreditsPatches.cs
+++ SCHIZO/Credits/CreditsPatches.cs
@@ -16,36 +16,12 @@ public static class CreditsPatches
 {
     private static readonly Dictionary<string, string> _credits = new()
     {
-        ["2Pfrog"] = "2D Art",
-        ["AlexejheroDev"] = "Project Lead, Programming, Prefab Setup",
-        ["Azit"] = "2D Art",
-        ["baa14453"] = "Lore",
-        ["budwheizzah"] = "Programming, Prefab Setup, 2D Art, Sounds",
-        ["chrom"] = "2D Art",
-        ["CJMAXiK"] = "2D Art, Sounds",
-        ["darkeew"] = "Prefab Setup",
-        ["FutabaKuuhaku"] = "3D Modeling",
-        ["Govorunb"] = "Programming",
-        ["greencap"] = "3D Modeling",
-        ["Hakuhan"] = "2D Art",
-        ["Kat"] = "3D Modeling",
-        ["Kaz"] = "2D Art",
-        ["Lorx"] = "2D Art",
-        ["Moloch"] = "2D Art",
-        ["MyBraza"] = "2D Art, Sounds",
-        ["NetPlayz"] = "2D Art",
-        ["P3R"] = "2D Art",
-        ["paccha"] = "2D Art",
-        ["Rune"] = "2D Art",
-        ["SADecsSs"] = "2D Art",
-        ["Sandro"] = "2D Art",
-        ["SomeOldGuy"] = "2D Art",
-        ["sugarph"] = "2D Art",
-        ["Troobs"] = "2D Art",
-        ["Vaalmyr"] = "3D Modeling",
-        ["w1n7er"] = "3D Modeling, Animations",
-        ["yamplum"] = "2D Art, Lore",
-        ["YuG"] = "3D Modeling",
+        ["DrGlitchy"] = "The only guy who did anything",
+        ["2Pfrog"] = "Can't type in chat",
+        ["AlexejheroDev"] = "Can't do anything without Darkeewe",
+        ["budwheizzah"] = "Kirby",
+        ["Govorunb"] = "Has a weird pfp",
+
     };
 
     [HarmonyPatch(typeof(EndCreditsManager), nameof(EndCreditsManager.Start))]
