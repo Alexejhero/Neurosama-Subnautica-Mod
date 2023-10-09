bbd98819871e7bcbbd2c5039d49f25fbde7e9e51
Govorunb <Govorunb@users.noreply.github.com>
2023-10-09 17:28:30 +1100

    battlepass fumo

--- SCHIZO/Items/FumoItem/FumoItemTool.cs
+++ SCHIZO/Items/FumoItem/FumoItemTool.cs
@@ -8,6 +8,7 @@ public sealed partial class FumoItemTool : CustomPlayerTool
     private float hugDistScale;
     private const float hugCooldown = 1f;
     private float nextHugTime;
+    private readonly Vector3 chestOffset = new(0, -0.3f, 0);
 
     private bool isHugging;
     private const float hugDistance = 0.5f;
@@ -15,6 +16,10 @@ public sealed partial class FumoItemTool : CustomPlayerTool
     private const float hugMoveSpeedMulti = 0.7f;
     private const int hugColdResistBuff = 20;
     private bool hugEffectApplied;
+
+    private bool canFlush;
+    private bool isFlushed;
+    private const float flushedZscalar = 2f;
 
     private GroundMotor groundMotor;
 
@@ -25,43 +30,15 @@ public sealed partial class FumoItemTool : CustomPlayerTool
         primaryUseTextLanguageString = "Hug ({0})";
         holsterTime = 0.1f;
 
-#warning DEV
-        hasAltUse = true;
-        altUseTextLanguageString = "Face me ({0})";
+        canFlush = Random.Range(0f, 1f) < 0.05f;
+        if (canFlush)
+        {
+            hasAltUse = true;
+            altUseTextLanguageString = "Flushed ({0})";
+        }
 
         base.Awake();
     }
-    public override void OnHolster()
-    {
-        // need to reset immediately, otherwise PDA opens in the wrong location
-        UpdateHugPos(0);
-        StopHugging();
-        base.OnHolster();
-    }
-
-    public override bool OnRightHandDown() => false; // don't play floater release anim
-
-    public override bool OnRightHandHeld()
-    {
-        if (!isHugging && Time.time > nextHugTime)
-            StartHugging();
-        return base.OnRightHandHeld();
-    }
-
-    public override bool OnRightHandUp()
-    {
-        StopHugging();
-        return base.OnRightHandUp();
-    }
-
-    public override bool OnAltHeld()
-    {
-        Transform vm = transform.Find("VM");
-        vm.LookAt(usingPlayer.transform.position with { y = vm.position.y });
-        return base.OnAltHeld();
-    }
-
-    private Vector3 chestOffset = new(0, -0.3f, 0);
 
     public void Update()
     {
@@ -82,6 +59,48 @@ public sealed partial class FumoItemTool : CustomPlayerTool
         Vector3 delta = offset - prevHugPosOffset;
         parent.localPosition += delta;
         prevHugPosOffset = offset;
+    }
+
+    public override void OnHolster()
+    {
+        // need to reset immediately, otherwise PDA opens in the wrong location
+        UpdateHugPos(0);
+        StopHugging();
+        if (isFlushed) ApplyZScaleMulti(1f / flushedZscalar);
+        base.OnHolster();
+    }
+
+    public override bool OnRightHandDown() => false; // don't play floater release anim
+
+    public override bool OnRightHandHeld()
+    {
+        if (!isHugging && Time.time > nextHugTime)
+            StartHugging();
+        return base.OnRightHandHeld();
+    }
+
+    public override bool OnRightHandUp()
+    {
+        StopHugging();
+        return base.OnRightHandUp();
+    }
+
+    public override bool OnAltDown()
+    {
+        if (!canFlush || isFlushed) return false;
+
+        isFlushed = true;
+        ApplyZScaleMulti(flushedZscalar);
+        return base.OnAltDown();
+    }
+
+    public override bool OnAltUp()
+    {
+        if (!canFlush || !isFlushed) return false;
+
+        isFlushed = false;
+        ApplyZScaleMulti(1f / flushedZscalar);
+        return base.OnAltUp();
     }
 
     public void StartHugging()
@@ -117,5 +136,13 @@ public sealed partial class FumoItemTool : CustomPlayerTool
         groundMotor.strafeMaxSpeed *= multi;
         groundMotor.backwardMaxSpeed *= multi;
         groundMotor.GetComponent<UnderwaterMotor>().debugSpeedMult *= multi;
+    }
+
+    private void ApplyZScaleMulti(float multi)
+    {
+        Transform vm = transform.Find("VM/neurofumo new");
+        Vector3 scale = vm.localScale;
+        scale.z *= multi;
+        vm.localScale = scale;
     }
 }
