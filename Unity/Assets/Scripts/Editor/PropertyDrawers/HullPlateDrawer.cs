59b60fdf9ee9d2271dbb3849c718da6badab369a
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-10-10 16:11:24 +0300

    quick hull plate editor

---
+++ Unity/Assets/Scripts/Editor/PropertyDrawers/HullPlateDrawer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using SCHIZO.Unity.HullPlates;
+using UnityEditor;
+using UnityEngine;
+
+namespace PropertyDrawers
+{
+    [CustomPropertyDrawer(typeof(HullPlate))]
+    public sealed class HullPlateDrawer : PropertyDrawer
+    {
+        public static List<int> NormalHullPlates = new List<int>();
+        public static List<int> DeprecatedHullPlates = new List<int>();
+
+        private static bool IsOk(SerializedProperty property)
+        {
+            if (!property.objectReferenceValue) return true;
+
+            int instanceId = property.objectReferenceInstanceIDValue;
+            bool isDeprecated = ((HullPlate) property.objectReferenceValue).deprecated;
+
+            if (NormalHullPlates.Contains(instanceId) && isDeprecated) return false;
+            if (DeprecatedHullPlates.Contains(instanceId) && !isDeprecated) return false;
+            return true;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Color oldColor = GUI.backgroundColor;
+            if (!IsOk(property)) GUI.backgroundColor = Color.red;
+
+            EditorGUI.PropertyField(position, property, label);
+
+            GUI.backgroundColor = oldColor;
+
+            property.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
