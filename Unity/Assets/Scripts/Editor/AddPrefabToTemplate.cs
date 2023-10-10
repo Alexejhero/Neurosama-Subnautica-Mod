e37aad94a942c0451b9c60df2dda2d5a8689586b
w1n7er87 <alexn78due@gmail.com>
2023-10-10 18:09:27 +0200

    Update AddPrefabToTemplate.cs

--- Unity/Assets/Scripts/Editor/AddPrefabToTemplate.cs
+++ Unity/Assets/Scripts/Editor/AddPrefabToTemplate.cs
@@ -1,29 +1,37 @@
 using UnityEditor;
 using UnityEngine;
 using System.IO;
+
 public class AddPrefabToTemplate
 {
     private static string templateFolderPath = "Assets/Templates/";
+    private enum types
+    {
+        Creature,
+        Constructable,
+    }
+
+    private static void createPrefab(types t, string localPath)
+    {
+        string outputFolderPath = Path.GetDirectoryName(localPath) + "/";
+        GameObject selectedObject = PrefabUtility.LoadPrefabContents(localPath);
+        GameObject template = (GameObject) PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<GameObject>(templateFolderPath + t.ToString() + " Prefab Template.prefab"));
+        string resultPrefabNameAndPath = AssetDatabase.GenerateUniqueAssetPath(outputFolderPath + selectedObject.name + ".prefab");
+        selectedObject.transform.SetParent(template.transform, false);
+        PrefabUtility.SaveAsPrefabAssetAndConnect(template, resultPrefabNameAndPath, InteractionMode.AutomatedAction);
+        GameObject.DestroyImmediate(template);
+    }
 
     [MenuItem("Assets/Create Creature Prefab")]
     public static void CreateCreaturePrefab()
     {
-        string selectionPath = AssetDatabase.GetAssetPath(Selection.activeGameObject);
-        string outputFolderPath = Path.GetDirectoryName(selectionPath) + "/";
-
-        GameObject selectedObject = PrefabUtility.LoadPrefabContents(selectionPath);
-        GameObject template = (GameObject) PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<GameObject>(templateFolderPath + "Creature Prefab Template.prefab"));
-        string newAssetPath = AssetDatabase.GenerateUniqueAssetPath(outputFolderPath + selectedObject.name + ".prefab");
-        selectedObject.transform.SetParent(template.transform, false);
-        PrefabUtility.SaveAsPrefabAssetAndConnect(template, newAssetPath, InteractionMode.UserAction);
-        GameObject.DestroyImmediate(template);
-        //PrefabUtility.UnloadPrefabContents(selectedObject); ???????
+        createPrefab(types.Creature, AssetDatabase.GetAssetPath(Selection.activeGameObject));
     }
 
     [MenuItem("Assets/Create Creature Prefab", true)]
     public static bool ValidateCreateCreaturePrefab()
     {
-        if (Selection.activeObject != null && Selection.objects.Length == 1 && Selection.activeObject.GetType().ToString() == "UnityEngine.GameObject")
+        if (Selection.activeObject != null && Selection.objects.Length == 1 && Selection.activeObject is GameObject)
             return true;
         return false;
     }
@@ -31,22 +39,13 @@ public class AddPrefabToTemplate
     [MenuItem("Assets/Create Constructable Prefab")]
     public static void CreateConstructablePrefab()
     {
-        string selectionPath = AssetDatabase.GetAssetPath(Selection.activeGameObject);
-        string outputFolderPath = Path.GetDirectoryName(selectionPath) + "/";
-
-        GameObject selectedObject = PrefabUtility.LoadPrefabContents(selectionPath);
-        GameObject template = (GameObject) PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<GameObject>(templateFolderPath + "Constructable Prefab Template.prefab"));
-        string newAssetPath = AssetDatabase.GenerateUniqueAssetPath(outputFolderPath + selectedObject.name + ".prefab");
-        selectedObject.transform.SetParent(template.transform, false);
-        PrefabUtility.SaveAsPrefabAssetAndConnect(template, newAssetPath, InteractionMode.UserAction);
-        GameObject.DestroyImmediate(template);
-        //PrefabUtility.UnloadPrefabContents(selectedObject); ?????
+        createPrefab(types.Constructable, AssetDatabase.GetAssetPath(Selection.activeGameObject));
     }
 
     [MenuItem("Assets/Create Constructable Prefab", true)]
     public static bool ValidateCreateConstructablePrefab()
     {
-        if (Selection.activeObject != null && Selection.objects.Length == 1 && Selection.activeObject.GetType().ToString() == "UnityEngine.GameObject")
+        if (Selection.activeObject != null && Selection.objects.Length == 1 && Selection.activeObject is GameObject)
             return true;
         return false;
     }
