435d7087100fe574e59fd3756934edddd902d3ea
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-10-11 19:22:11 +0300

    add TrailManager in unity

--- Unity/Assets/Scripts/Subnautica/AnimateByVelocity.cs
+++ Unity/Assets/Scripts/Subnautica/AnimateByVelocity.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class AnimateByVelocity : MonoBehaviour
 {
-    [Foldout(STRINGS.COMPONENT_REFERENCES), Required] public GameObject rootGameObject;
-    [Foldout(STRINGS.COMPONENT_REFERENCES), Required] public BehaviourLOD levelOfDetail;
-
+    [Required] public GameObject rootGameObject;
     [Required] public Animator animator;
+
     public float animationMoveMaxSpeed = 4;
     public float animationMaxPitch = 30;
     public float animationMaxTilt = 45;
     public bool useStrafeAnimation = false;
     public float dampTime = 0.5f;
+
+    [Foldout(STRINGS.COMPONENT_REFERENCES), Required] public BehaviourLOD levelOfDetail;
 }
---
+++ Unity/Assets/Scripts/Subnautica/TrailManager.cs
@@ -0,0 +1,18 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+public class TrailManager : MonoBehaviour
+{
+    [Required] public Transform rootTransform;
+
+    public Transform rootSegment;
+    public Transform[] trails;
+    public AnimationCurve pitchMultiplier;
+    public AnimationCurve rollMultiplier;
+    public AnimationCurve yawMultiplier;
+    public float segmentSnapSpeed;
+    public float maxSegmentOffset;
+    public bool allowDisableOnScreen;
+
+    [Foldout(STRINGS.COMPONENT_REFERENCES), Required] public BehaviourLOD levelOfDetail;
+}
