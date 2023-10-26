9ecb2d0416d75344e78eaf4122111653fca7803f
Govorunb <Govorunb@users.noreply.github.com>
2023-10-26 12:44:53 +1100

    assets
    + remove error spam from broken loading patches

--- SCHIZO/Resources/AssetBundles/Assets.cs
+++ SCHIZO/Resources/AssetBundles/Assets.cs
@@ -12,7 +12,7 @@ namespace SCHIZO.Resources;
 
 public static class Assets
 {
-    private const int _rnd = -1420678272;
+    private const int _rnd = 123456789;
 
     private static readonly UnityEngine.AssetBundle _a = ResourceManager.GetAssetBundle("assets");
 
