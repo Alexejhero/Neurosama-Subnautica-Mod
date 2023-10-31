a0ceda63a2565f7fcb7920809f1dae2d66a4bdd7
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2023-10-31 03:33:51 +0200

    start working on custom console commands

---
+++ SCHIZO/Console/BetterThanNautilus/ConsoleCommandEx.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Nautilus.Commands;
+
+[assembly: IgnoresAccessChecksTo("Nautilus")]
+
+namespace SCHIZO.Console.BetterThanNautilus;
+
+public class ConsoleCommandEx : ConsoleCommand
+{
+    public ConsoleCommandEx(string trigger, MethodInfo targetMethod, bool isDelegate = false, object instance = null) : base(trigger, targetMethod, isDelegate, instance)
+    {
+    }
+}
