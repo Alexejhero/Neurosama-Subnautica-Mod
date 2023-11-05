84a794102a220187b7e573caaf2c995daf6be164
Govorunb <Govorunb@users.noreply.github.com>
2023-11-06 01:35:53 +1100

    tiny tiny thing

--- Unity/Assets/Scripts/SCHIZO/Creatures/Components/CustomCreatureTool.cs
+++ Unity/Assets/Scripts/SCHIZO/Creatures/Components/CustomCreatureTool.cs
@@ -1,17 +1,18 @@
-﻿using NaughtyAttributes;
+using TriInspector;
 using SCHIZO.Interop.Subnautica;
 using SCHIZO.Interop.Subnautica.Enums;
 using UnityEngine;
 
 namespace SCHIZO.Creatures.Components
 {
+    [DeclareBoxGroup("ik_bz", Title = "IK Overrides (Below Zero)")]
     public partial class CustomCreatureTool : _CreatureTool
     {
         [SerializeField] private TechType_All referenceAnimation;
         [SerializeField] private GameObject subnauticaModel;
         [SerializeField] private GameObject belowZeroModel;
 
-        [Foldout("IK"), SerializeField] private Transform leftHandIKTargetOverrideBZ;
-        [Foldout("IK"), SerializeField] private Transform rightHandIKTargetOverrideBZ;
+        [Group("ik_bz"), LabelText("Left Hand IK Target"), SerializeField] private Transform leftHandIKTargetOverrideBZ;
+        [Group("ik_bz"), LabelText("Right Hand IK Target"), SerializeField] private Transform rightHandIKTargetOverrideBZ;
     }
 }
