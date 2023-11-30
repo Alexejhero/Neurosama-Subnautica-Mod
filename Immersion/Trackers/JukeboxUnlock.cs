46409741878eef8a946b35c013ec3c458b319eed
Govorunb <Govorunb@users.noreply.github.com>
2023-12-01 01:33:06 +1100

    react to tfm unlock

---
+++ Immersion/Trackers/JukeboxUnlock.cs
@@ -0,0 +1,30 @@
+using HarmonyLib;
+using Immersion.Formatting;
+
+namespace Immersion.Trackers;
+
+[HarmonyPatch]
+public sealed class JukeboxUnlock : Tracker
+{
+    internal static HashSet<string> _allowlist = new(StringComparer.InvariantCultureIgnoreCase)
+    {
+        "truckersfm"
+    };
+
+    internal void NotifyUnlocked(Jukebox.UnlockableTrack track)
+    {
+        if (!Jukebox.unlockableMusic.TryGetValue(track, out string name)) return;
+        if (!_allowlist.Contains(name)) return;
+
+        React(Priority.Low, Format.FormatPlayer($"{{player}} has unlocked a new jukebox track: {Jukebox.GetInfo(name).label}"));
+    }
+
+    [HarmonyPatch(typeof(Jukebox), nameof(Jukebox.OnUnlock))]
+    [HarmonyPostfix]
+    private static void OnUnlock(Jukebox.UnlockableTrack track, bool notify)
+    {
+        if (!notify) return;
+
+        COMPONENT_HOLDER.GetComponent<JukeboxUnlock>().NotifyUnlocked(track);
+    }
+}
