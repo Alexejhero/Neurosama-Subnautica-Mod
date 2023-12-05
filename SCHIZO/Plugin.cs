fd71c911d96427d031812623b957cbd326901b48
Govorunb <Govorunb@users.noreply.github.com>
2023-12-05 15:27:46 +1100

    load the banks slightly better

--- SCHIZO/Plugin.cs
+++ SCHIZO/Plugin.cs
@@ -34,33 +34,13 @@ public sealed class Plugin : BaseUnityPlugin
         HARMONY.PatchAll();
     }
 
-    private void LoadBank(string file, bool loadStrings = true)
-    {
-        byte[] fmodBank = ResourceManager.GetEmbeddedBytes($"{file}.bank", true);
-        RESULT res = RuntimeManager.StudioSystem.loadBankMemory(fmodBank, FMOD.Studio.LOAD_BANK_FLAGS.NORMAL, out FMOD.Studio.Bank bank);
-        res.CheckResult();
-        if (!bank.hasHandle())
-        {
-            throw new BankLoadException(file, res);
-        }
-        if (loadStrings) LoadBank($"{file}.strings", false);
-    }
-
     private IEnumerator Start()
     {
         yield return ObjectReferences.SetReferences();
         yield return MaterialHelpers.LoadMaterials();
         StaticHelpers.CacheAttribute.CacheAll();
-
-        try
-        {
-            LoadBank("SCHIZO");
-        }
-        catch (BankLoadException e)
-        {
-            LOGGER.LogError($"Could not load FMOD bank {e}");
-        }
-
+        FMODHelpers.LoadMasterBank("SCHIZO");
+        
         Assets.Mod_Registry.InvokeRegister();
         Assets.Mod_Registry.InvokePostRegister();
 
