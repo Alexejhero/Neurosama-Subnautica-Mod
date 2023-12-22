063dd489f8a1f6b96ac4264f1a76f1c20ff00494
Govorunb <Govorunb@users.noreply.github.com>
2023-12-22 22:52:01 +1100

    commit assets

--- SCHIZO/Resources/AssetBundles/Assets.cs
+++ SCHIZO/Resources/AssetBundles/Assets.cs
@@ -12,7 +12,7 @@ namespace SCHIZO.Resources;
 
 public static class Assets
 {
-    private const int _rnd = -357486816;
+    private const int _rnd = 1956607609;
 
     private static readonly UnityEngine.AssetBundle _a = ResourceManager.GetAssetBundle("assets");
 
