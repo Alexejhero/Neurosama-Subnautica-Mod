6a353c2b3fc92902a99e8292145c6abf204ba742
Govorunb <Govorunb@users.noreply.github.com>
2024-02-03 12:57:34 +1100

    respect sound options

--- Unity/Assets/Scripts/SCHIZO/Sounds/Players/HurtSoundPlayer.cs
+++ Unity/Assets/Scripts/SCHIZO/Sounds/Players/HurtSoundPlayer.cs
@@ -1,7 +1,13 @@
+using FMODUnity;
+using JetBrains.Annotations;
+using UnityEngine;
+
 namespace SCHIZO.Sounds.Players
 {
     public sealed partial class HurtSoundPlayer : SoundPlayer
     {
         protected override bool Is3D => true;
+        [EventRef, SerializeField, UsedImplicitly]
+        private string deathSound;
     }
 }
