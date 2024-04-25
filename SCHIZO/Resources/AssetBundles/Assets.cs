29ee97e808a3250d3822170cc9e718c62907e414
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-04-25 19:16:45 +0200

    no mipmaps please :(

--- SCHIZO/Resources/AssetBundles/Assets.cs
+++ SCHIZO/Resources/AssetBundles/Assets.cs
@@ -12,7 +12,7 @@ namespace SCHIZO.Resources;
 
 public static class Assets
 {
-    private const int _rnd = -1737235644;
+    private const int _rnd = 100786890;
 
     private static readonly UnityEngine.AssetBundle _a = ResourceManager.GetAssetBundle("assets");
 
