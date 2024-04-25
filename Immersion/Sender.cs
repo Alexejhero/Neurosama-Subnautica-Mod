122edc3bf5161c8e8dce15894f6936c2f1e8965a
Govorunb <Govorunb@users.noreply.github.com>
2024-04-26 01:32:38 +1000

    live hotfix

--- Immersion/Sender.cs
+++ Immersion/Sender.cs
@@ -1,33 +1,21 @@
 using System.Collections;
-using System.Net.Http;
 using UnityEngine.Networking;
 
 namespace Immersion;
 
 internal class Sender : MonoBehaviour
 {
-    private static HttpClient _client;
-
     /// <summary>
     /// Force the next <see cref="Send"/> call to proceed even if the component is disabled.
     /// </summary>
     public bool forceNext;
-    private Uri _baseUri;
-
-    public void Awake()
-    {
-        _baseUri = new(Globals.BaseUrl);
-        _client = new HttpClient { BaseAddress = new Uri(Globals.BaseUrl) };
-        _client.DefaultRequestHeaders.UserAgent.Add(new("Unity", Application.unityVersion));
-        _client.DefaultRequestHeaders.UserAgent.Add(new("(SCHIZO.Immersion)"));
-    }
 
     public void Send(string path, object data = null)
     {
         if (!enabled && !forceNext) return;
         forceNext = false;
 
-        Uri uri = new(_baseUri, path);
+        Uri uri = new(new(Globals.BaseUrl), path);
         string postData = data?.ToString() ?? "";
         StartCoroutine(SendCoro(uri, postData));
     }
