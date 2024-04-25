faa25cfbeec0011ca7a783934d6c694ec7ccd40a
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-04-25 18:57:19 +0200

    initialization thingies + seatruck sleeper rename

---
+++ SCHIZO/Tweaks/SeatruckSleeperModuleRename.BelowZero.cs
@@ -0,0 +1,16 @@
+﻿using Nautilus.Handlers;
+using SCHIZO.Attributes;
+
+namespace SCHIZO.Tweaks;
+
+public static class SeatruckSleeperModuleRename
+{
+    [InitializeMod]
+    public static void Apply()
+    {
+        LanguageHandler.SetTechTypeName(TechType.SeaTruckSleeperModule, "Seatruck Jukebox Module");
+        LanguageHandler.SetTechTypeName(TechType.SeaTruckSleeperModuleFragment, "Seatruck Jukebox Module Fragment");
+        LanguageHandler.SetLanguageLine("Ency_SeaTruckSleeperModule", "Seatruck Jukebox Module");
+        LanguageHandler.SetLanguageLine("PilotSeaTruckSleeperModule", "Pilot Jukebox Module");
+    }
+}
