5b65fafda252276d6244dd36fa9acc9d8efae754
Govorunb <Govorunb@users.noreply.github.com>
2024-05-17 13:28:20 +1000

    add ermshark egg

--- SCHIZO/Items/Components/ModdedCreatureEgg.cs
+++ SCHIZO/Items/Components/ModdedCreatureEgg.cs
@@ -4,19 +4,15 @@ namespace SCHIZO.Items.Components;
 
 partial class ModdedCreatureEgg
 {
+    private CreatureEgg Egg => creatureEgg as CreatureEgg;
     private void Awake()
     {
-        CreatureEgg egg = gameObject.AddComponent<CreatureEgg>();
-        egg.creaturePrefab = new CustomGameObjectReference(creature.GetClassID());
-        egg.creatureType = creature.GetTechType();
-        egg.daysBeforeHatching = daysBeforeHatching;
-#if BELOWZERO
-        egg.animators = [animator];
-        egg.animateOutside = true; // todo
-#else
-        egg.animator = animator;
-#endif
-        egg.liveMixin = (LiveMixin)liveMixin;
-        // TODO: hatching progress deserialization
+        if (!Egg)
+        {
+            LOGGER.LogWarning($"{nameof(creatureEgg)} was not assigned to {this}, creating new - deserialization (e.g. loading hatching progress from save) will fail!");
+            creatureEgg = gameObject.AddComponent<CreatureEgg>();
+        }
+        Egg.creaturePrefab = new CustomGameObjectReference(creature.GetClassID());
+        Egg.creatureType = creature.GetTechType();
     }
 }
--- Unity/Assets/Scripts/SCHIZO/Items/Components/ModdedCreatureEgg.cs
+++ Unity/Assets/Scripts/SCHIZO/Items/Components/ModdedCreatureEgg.cs
@@ -9,13 +9,9 @@ namespace SCHIZO.Items.Components
     [DisallowMultipleComponent]
     public sealed partial class ModdedCreatureEgg : MonoBehaviour
     {
-        [ExposedType("LiveMixin"), SerializeField, UsedImplicitly, Required]
-        private MonoBehaviour liveMixin;
+        [ExposedType("CreatureEgg"), SerializeField, UsedImplicitly, Required]
+        private MonoBehaviour creatureEgg;
         [SerializeField, UsedImplicitly]
         private Item creature;
-        [SerializeField, UsedImplicitly]
-        private float daysBeforeHatching = 1f;
-        [SerializeField, UsedImplicitly, Required]
-        private Animator animator;
     }
 }
