21cad8516202e03d3b3d5aecee46499245d9b1c7
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-05-17 06:38:54 +0200

    program in c

--- SCHIZO/Resources/AssetBundles/Assets.cs
+++ SCHIZO/Resources/AssetBundles/Assets.cs
@@ -12,7 +12,7 @@ namespace SCHIZO.Resources;
 
 public static class Assets
 {
-    private const int _rnd = 1060035311;
+    private const int _rnd = 813696171;
 
     private static readonly UnityEngine.AssetBundle _a = ResourceManager.GetAssetBundle("assets");
 
